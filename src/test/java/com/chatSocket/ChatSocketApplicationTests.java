f172816b1eca063b68d7b4204c9d26316274acf7
Ahmed.Elsheikh <ahmed.elsheikh@AlAhlyMomkn.com>
2025-05-20 23:31:17 +0300

    init project

---
+++ src/test/java/com/chatSocket/ChatSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
