d864178e0c228bfb47711c675670292c54567050
Ahmed.Elsheikh <ahmed.elsheikh@AlAhlyMomkn.com>
2025-05-21 00:24:18 +0300

    create web socket config

--- src/main/java/com/chatSocket/config/WebSocketConfig.java
+++ src/main/java/com/chatSocket/config/WebSocketConfig.java
@@ -1,6 +1,5 @@
 package com.chatSocket.config;
 
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
