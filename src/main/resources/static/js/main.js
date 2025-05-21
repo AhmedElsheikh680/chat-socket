e45aa857576ccec4a2d6d88f898ab7fcc61f624b
Ahmed.Elsheikh <ahmed.elsheikh@AlAhlyMomkn.com>
2025-05-21 23:38:21 +0300

    chat join user

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,8 @@
 var loginElelent = document.querySelector('#login');
 var chatElement = document.querySelector('#chat');
 var userForm = document.querySelector('#userForm');
+var connect = document.querySelector('#connect');
+var mainChat = document.querySelector('#main-chat');
 var URL = "http://localhost:8080";
 var userName=null
 var stomp = null;
@@ -18,10 +20,29 @@ function connectSocket(event) {
 }
 
 function connectedDone() {
-    stomp.send("/app/chat.login", {}, JSON.stringify({message: userName, chatType: 'JOIN'}))
-    stomp.subscribe("/app/chat.send", sendMessage)
+    stomp.subscribe("/topic/all", sendMessage)
+    stomp.send("/app/chat.login", {}, JSON.stringify({sender: userName, chatType: 'JOIN'}))
+    connect.classList.add("dis")
 }
-function sendMessage() {
+function sendMessage(payload) {
+    var message = JSON.parse(payload.body)
+    if (message.chatType == 'JOIN') {
+        joinUser(message)
+    }
 
+}
+
+function joinUser(message) {
+    var li1 = document.createElement('li');
+    var li2 = document.createElement('li');
+    var hr1 = document.createElement('hr');
+    var hr2 = document.createElement('hr');
+    var messageJoin = document.createTextNode(message.sender + ' Join')
+    li1.classList.add('status');
+    li1.appendChild(messageJoin)
+    li2.appendChild(hr1)
+    li2.appendChild(li1)
+    li2.appendChild(hr2)
+    mainChat.appendChild(li2)
 }
 userForm.addEventListener('submit', connectSocket)
